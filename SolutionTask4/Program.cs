24e8f8495cfa01af1426ddfaf38f188e53e321fd
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 11:48:35 +0300

    add Solution Task 4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+
+    Console.WriteLine(M[2]);
+
+    // int inputNumber = int.Parse(inputLine);
+
+    // int outputNumber = inputNumber%10;
+
+    // Console.WriteLine(outputNumber);
+}
+
