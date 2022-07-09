9d70f050078c51314f5d00ce7834b83830e8d888
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 12:23:36 +0300

    add solution task 6

---
+++ SolutionTask6/Program.cs
@@ -0,0 +1,29 @@
+﻿string? inputLine1 = Console.ReadLine();
+string? inputLine2 = Console.ReadLine();
+string? inputLine3 = Console.ReadLine();
+
+if(inputLine1 != null && inputLine2 != null  && inputLine3 != null)
+{
+    int inputNumber1 = int.Parse(inputLine1);
+    int inputNumber2 = int.Parse(inputLine2);
+    int inputNumber3 = int.Parse(inputLine3);
+
+    if(inputNumber1 > inputNumber2)
+    {
+        if(inputNumber1 > inputNumber3);
+
+        Console.WriteLine("inputNumber1");
+    }
+        else
+            {
+            if(inputNumber2 > inputNumber3)
+
+            Console.WriteLine("inputNumber2");
+            
+            else
+            
+            Console.WriteLine("inputNumber3");
+            }
+
+}
+
