dbeaf1b359f304ff369875b70445b88bff692192
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 11:08:23 +0300

    add solution task 3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inpuntNumber = int.Parse(inputLine);
+
+    int startNumber = (inpuntNumber * -1);
+
+    string lineOutput = "";
+
+    while (startNumber < inpuntNumber)
+    {
+        lineOutput = lineOutput + startNumber + ", ";
+        startNumber++;
+    }
+
+    lineOutput = lineOutput + inpuntNumber;
+
+    Console.WriteLine(lineOutput);
+
+}
