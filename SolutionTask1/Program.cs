45663ff7ca4721967b9e9dda31b1aa831f67afce
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 10:11:29 +0300

    add solution task v2

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -6,7 +6,8 @@ if(inputLine1 != null && inputLine2 != null)
     int inputNumber1 = int.Parse(inputLine1);
     int inputNumber2 = int.Parse(inputLine2);
 
-    if(inputNumber1 == inputNumber2*inputNumber2)
+    //if(inputNumber2 == inputNumber1*inputNumber1) - 1 вариант решения задачи
+    if(inputNumber2 == Math.Sqrt(inputNumber1))
     {
         Console.WriteLine("yes");
     }
