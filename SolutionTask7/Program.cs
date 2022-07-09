12f2a12b8758c649eb31f99b44820bc0002ed701
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 13:12:46 +0300

    add solution task 7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int outNumber = inputNumber%2;
+
+    if(outNumber == 0)
+    {
+    Console.WriteLine("yes");
+    }
+        else
+    {
+        Console.WriteLine("no");
+    }
+
+}
