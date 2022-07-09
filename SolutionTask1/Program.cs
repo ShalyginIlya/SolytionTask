70f60a7a2beeb723f1e2750ba3c082ef5400cc4d
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 10:02:41 +0300

    add solution task 1

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -1,14 +1,18 @@
 ﻿string? inputLine1 = Console.ReadLine();
 string? inputLine2 = Console.ReadLine();
 
-if(inputLine1 != null && nputLine2 != null){
+if(inputLine1 != null && inputLine2 != null)
+{
     int inputNumber1 = int.Parse(inputLine1);
     int inputNumber2 = int.Parse(inputLine2);
 
-    if(inputNumber1 == inputNumber2*inputNumber2){
-        Console.WriteLine("Yes");
-    }else{
-        Console.WriteLine("No")
+    if(inputNumber1 == inputNumber2*inputNumber2)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
     }
 
 }
