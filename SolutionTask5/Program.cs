057c7c2a2f29aace9f14c25e4c097d18d4c6937a
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 11:56:39 +0300

    add solution task 5

---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputLine1 = Console.ReadLine();
+string? inputLine2 = Console.ReadLine();
+
+if(inputLine1 != null && inputLine2 != null)
+{
+    int inputNumber1 = int.Parse(inputLine1);
+    int inputNumber2 = int.Parse(inputLine2);
+
+    if(inputNumber1 > inputNumber2)
+    {
+        Console.WriteLine("inputNumber1");
+    }
+    else
+    {
+        Console.WriteLine("inputNumber2");
+    }
+
+}
