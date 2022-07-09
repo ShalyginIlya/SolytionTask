86831c11c6c6780971085c1898df5dd94c2ee29c
«Ilia» <shalygin.ii@yandex.ru>
2022-07-09 11:23:51 +0300

    add solytion task 3 v2

--- SolutionTask3/Program.cs
+++ SolutionTask3/Program.cs
@@ -4,17 +4,29 @@ if(inputLine != null)
 {
     int inpuntNumber = int.Parse(inputLine);
 
-    int startNumber = (inpuntNumber * -1);
+    int startNumber = 1;
 
-    string lineOutput = "";
+    string lineOutput = "0";
 
-    while (startNumber < inpuntNumber)
+    while (startNumber <= inpuntNumber)
     {
-        lineOutput = lineOutput + startNumber + ", ";
+        lineOutput = startNumber*(-1) + ", " + lineOutput + ", " + startNumber;
         startNumber++;
     }
 
-    lineOutput = lineOutput + inpuntNumber;
+    // lineOutput = lineOutput + inpuntNumber;
+
+    // int startNumber = (inpuntNumber * -1);
+
+    // string lineOutput = "";
+
+    // while (startNumber < inpuntNumber)
+    // {
+    //     lineOutput = lineOutput + startNumber + ", ";
+    //     startNumber++;
+    // }
+
+    // lineOutput = lineOutput + inpuntNumber;
 
     Console.WriteLine(lineOutput);
 
