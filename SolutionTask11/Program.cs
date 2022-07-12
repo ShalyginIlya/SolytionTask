77c39c0666bec5def63a10292c68d378ecdaf3c7
«Ilia» <shalygin.ii@yandex.ru>
2022-07-12 21:30:28 +0300

    сохр

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,33 @@
+﻿    string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null){
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+
+    int div = inputNumberTwo%inputNumberOne;
+
+    if(div == 0){
+        Console.WriteLine("yes");
+    }else{
+        Console.WriteLine(div);
+    }
+
+}
+
+// вариант2
+// Console.Write("Enter first number: ");
+// string? firstLine = Console.ReadLine();
+// Console.Write("Enter second number: ");
+// string? secondLine = Console.ReadLine();
+// int first = int.Parse(firstLine);
+// int second = int.Parse(secondLine);
+
+// if (second%first == 0)
+// {
+//     Console.WriteLine("Кратное");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратное, " + second%first);
+// }
