cdd034a8092fc24fe488aa0b6c821a94ae9993e3
«Ilia» <shalygin.ii@yandex.ru>
2022-07-12 21:56:50 +0300

    сохр

--- SolutionTask13/Program.cs
+++ SolutionTask13/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string? inputLkneOne = Console.ReadLine();
+// string? inputLkneTwo = Console.ReadLine();
+// if(inputLkneOne != null && inputLkneTwo != null ){
+//     int inputNumberOne = Math.Abs(int.Parse(inputLkneOne));
+//     int inputNumberTwo = Math.Abs(int.Parse(inputLkneTwo));
+//     double sq = 0;
+
+//     if(inputNumberOne > inputNumberTwo){
+//         sq = Math.Sqrt(inputNumberOne);
+//     }else{
+//         sq = Math.Sqrt(inputNumberTwo);
+//     }
+
+//     if(sq == inputNumberOne ^ sq == inputNumberTwo){
+//         Console.WriteLine("Да");
+//     }else{
+//         Console.WriteLine("Нет");
+//     }
+    
+// }
+
+//Вариант2
+
+Console.WriteLine("Введите первое число ");
+
+string? inputOne = Console.ReadLine();
+string? inputTwo = Console.ReadLine();
+
+if (inputOne != null && inputTwo != null)
+{
+    int numOne = int.Parse(inputOne);   
+    int numTwo = int.Parse(inputTwo);  
+
+    if (Math.Sqrt(numOne) == numTwo) 
+    {
+        Console.WriteLine("Yes");
+    } 
+    else if (Math.Sqrt(numTwo) == numOne) 
+    {
+        Console.WriteLine("Yes");   
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
