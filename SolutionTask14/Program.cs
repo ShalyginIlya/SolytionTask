c433ab7e4f54fe956748bb7fd6b09ec3d69782dd
«Ilia» <shalygin.ii@yandex.ru>
2022-07-13 22:24:01 +0300

    add solution task v4

--- SolutionTask14/Program.cs
+++ SolutionTask14/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 
 //Вариант 1
-string? inputLine = Console.ReadLine();
+string? inputLine = Console.ReadLine(); //Считываем строковую переменную
 
-int inputNumber = int.Parse(inputLine);
+int inputNumber = int.Parse(inputLine); //Превращаем строковую переменную в целочисленную
 
-int number = (inputNumber % 100) / 10;
+int number = (inputNumber % 100) / 10;  // (Получаем остаток от числа) полученный результат делим на 10
 
-Console.WriteLine(number);
+Console.WriteLine(number); // Выводим на консоль результат
 
 //Вариант 2
-string? inputLine1 = Console.ReadLine();
+string? inputLine1 = Console.ReadLine(); //Считываем строковую переменную
 
-char[] array = inputLine1.ToCharArray();
+char[] array = inputLine1.ToCharArray(); //Создаем массив array
 
-Console.WriteLine(array[1]);
+Console.WriteLine(array[1]); // Выводим на консоль число под индексом 1
