650c4490a4b4003965756f92f1f0701a843f4480
«Ilia» <shalygin.ii@yandex.ru>
2022-07-13 21:37:58 +0300

    add solution task 14 v3

--- SolutionTask14/Program.cs
+++ SolutionTask14/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
 
+//Вариант 1
 string? inputLine = Console.ReadLine();
 
 int inputNumber = int.Parse(inputLine);
@@ -7,3 +8,10 @@ int inputNumber = int.Parse(inputLine);
 int number = (inputNumber % 100) / 10;
 
 Console.WriteLine(number);
+
+//Вариант 2
+string? inputLine1 = Console.ReadLine();
+
+char[] array = inputLine1.ToCharArray();
+
+Console.WriteLine(array[1]);
