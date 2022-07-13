87929e88af7d0a6d70f2185dc693a0666479d192
«Ilia» <shalygin.ii@yandex.ru>
2022-07-13 20:30:26 +0300

    add solution task 11

--- SolutionTask11/Program.cs
+++ SolutionTask11/Program.cs
@@ -1,33 +1,41 @@
-﻿    string? inputLkneOne = Console.ReadLine();
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным 
+//первому. Если второе число некратно первому, то программа выводит о статок от деления
+
+//Вариант1
+string? inputLkneOne = Console.ReadLine();
 string? inputLkneTwo = Console.ReadLine();
 
-if(inputLkneOne != null && inputLkneTwo != null){
+if (inputLkneOne != null && inputLkneTwo != null)
+{
     int inputNumberOne = int.Parse(inputLkneOne);
     int inputNumberTwo = int.Parse(inputLkneTwo);
 
-    int div = inputNumberTwo%inputNumberOne;
+    int div = inputNumberTwo % inputNumberOne;
 
-    if(div == 0){
+    if (div == 0)
+    {
         Console.WriteLine("yes");
-    }else{
+    }
+    else
+    {
         Console.WriteLine(div);
     }
 
 }
 
-// вариант2
-// Console.Write("Enter first number: ");
-// string? firstLine = Console.ReadLine();
-// Console.Write("Enter second number: ");
-// string? secondLine = Console.ReadLine();
-// int first = int.Parse(firstLine);
-// int second = int.Parse(secondLine);
+// Вариант 2
+Console.Write("Enter first number: ");
+string? firstLine = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? secondLine = Console.ReadLine();
+int first = int.Parse(firstLine);
+int second = int.Parse(secondLine);
 
-// if (second%first == 0)
-// {
-//     Console.WriteLine("Кратное");
-// }
-// else
-// {
-//     Console.WriteLine("Не кратное, " + second%first);
-// }
+if (second%first == 0)
+{
+    Console.WriteLine("Кратное");
+}
+else
+{
+    Console.WriteLine("Не кратное, " + second%first);
+}
