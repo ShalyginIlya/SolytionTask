6fe4edad07ee3351a18324110e2d34c082656418
«Ilia» <shalygin.ii@yandex.ru>
2022-07-13 20:38:20 +0300

    add solution task 13

--- SolutionTask13/Program.cs
+++ SolutionTask13/Program.cs
@@ -1,23 +1,23 @@
-﻿// string? inputLkneOne = Console.ReadLine();
-// string? inputLkneTwo = Console.ReadLine();
-// if(inputLkneOne != null && inputLkneTwo != null ){
-//     int inputNumberOne = Math.Abs(int.Parse(inputLkneOne));
-//     int inputNumberTwo = Math.Abs(int.Parse(inputLkneTwo));
-//     double sq = 0;
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+if(inputLkneOne != null && inputLkneTwo != null ){
+    int inputNumberOne = Math.Abs(int.Parse(inputLkneOne));
+    int inputNumberTwo = Math.Abs(int.Parse(inputLkneTwo));
+    double sq = 0;
 
-//     if(inputNumberOne > inputNumberTwo){
-//         sq = Math.Sqrt(inputNumberOne);
-//     }else{
-//         sq = Math.Sqrt(inputNumberTwo);
-//     }
+    if(inputNumberOne > inputNumberTwo){
+        sq = Math.Sqrt(inputNumberOne);
+    }else{
+        sq = Math.Sqrt(inputNumberTwo);
+    }
 
-//     if(sq == inputNumberOne ^ sq == inputNumberTwo){
-//         Console.WriteLine("Да");
-//     }else{
-//         Console.WriteLine("Нет");
-//     }
+    if(sq == inputNumberOne ^ sq == inputNumberTwo){
+        Console.WriteLine("Да");
+    }else{
+        Console.WriteLine("Нет");
+    }
     
-// }
+}
 
 //Вариант2
 
