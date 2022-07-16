89d2a088c6db61c4d33f30f2f55c2179c82b5a40
«Ilia» <shalygin.ii@yandex.ru>
2022-07-16 10:27:58 +0300

    add solution task 19

---
+++ SolutionTask19/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+
+int coordXPointA;
+int coordXPointB;
+int coordYPointA;
+int coordYPointB;
+
+double lengthAB;
+
+//Считывает координаты точек А и В
+void readDataOfPoint()
+{
+    Console.WriteLine("Введите координату Х точки А");
+    coordXPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки А");
+    coordYPointA = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х точки B");
+    coordXPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB = int.Parse(Console.ReadLine());
+}
+
+//Вычисляет расстояние между точками А и В
+void conculateLengthAB()
+{
+   lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2)); 
+}
+
+readDataOfPoint();
+conculateLengthAB();
+
+Console.WriteLine(lengthAB);
+
+
+
+
+//Вариант без метода
+
+// Console.WriteLine("Введите координату Х точки А");
+// int coordXPointA = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y точки А");
+// int coordYPointA = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Х точки B");
+// int coordXPointB = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y точки B");
+// int coordYPointB = int.Parse(Console.ReadLine());
+
+// double lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
+
+// Console.WriteLine(lengthAB);
