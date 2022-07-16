23793c9d2fd2ca92c5fdef870981a456d7943f4a
«Ilia» <shalygin.ii@yandex.ru>
2022-07-16 09:47:38 +0300

    add solution task 18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+//Вариант с методом
+void printAnswer(int number)
+{
+    if (number == 1) Console.WriteLine("Допустимо: x>0, y>0");
+    if (number == 2) Console.WriteLine("Допустимо: x<0, y>0");
+    if (number == 3) Console.WriteLine("Допустимо: x<0, y<0");
+    if (number == 4) Console.WriteLine("Допустимо: x>0, y<0");
+}
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    printAnswer(inputNumber);
+}
+
+
+//Вариант без метода
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     int inputNumber = int.Parse(inputLine);
+
+//     if (inputNumber == 1) Console.WriteLine("Допустимо: x>0, y>0");
+//     if (inputNumber == 2) Console.WriteLine("Допустимо: x<0, y>0");
+//     if (inputNumber == 3) Console.WriteLine("Допустимо: x<0, y<0");
+//     if (inputNumber == 4) Console.WriteLine("Допустимо: x>0, y<0");
+// }
